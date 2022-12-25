01cda5633ae48376a29b040de4c9b19744c216e9
Wojciech Cymerys <wojciech.cymerys@onet.pl>
2022-12-25 09:04:00 +0100

    template commit

---
+++ src/main/java/pl/budowniczowie/tascocloud/TascoCloudApplication.java
@@ -0,0 +1,13 @@
+package pl.budowniczowie.tascocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TascoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TascoCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/budowniczowie/tascocloud/TascoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.budowniczowie.tascocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TascoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
